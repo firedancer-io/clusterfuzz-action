f5ddefb8084678dde3a943dbac388b5f0793b8a9
Marcus <117692400+marcus-jump@users.noreply.github.com>
2023-09-12 18:38:25 +0000

    bundle shared objects

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ import { Storage } from '@google-cloud/storage'
 import os from 'node:os'
 
 import getMakeVar from "./getMakeVar"
-import { stderr } from 'process'
+import { env, stderr } from 'process'
 
 let execp = promisify(exec)
 
@@ -23,6 +23,8 @@ async function run(): Promise<void> {
     const projectId: string = core.getInput('project-id')
     const serviceAccountCredentials: string = core.getInput('service-account-credentials')
     const artifactDir: string = core.getInput('artifact-dir')
+
+    console.log(`Working in ${__dirname}`)
 
     if (path.isAbsolute(artifactDir)) {
       throw new Error("cannot work with absolute paths")
@@ -39,30 +41,27 @@ async function run(): Promise<void> {
 
     // Copy fuzzing targets in staging
     await fsp.cp(artifactDir, fdfuzzdir, copyOptions);
-    // Merge seed corpus in staging
-    await fsp.cp("./corpus", fdfuzzdir, copyOptions);
+
+    // For each binary, rewrite the RPATH
+    let files = await fsp.readdir(fdfuzzdir)
+    for (let executable of files) {
+      await rewriteRPATH(path.join(fdfuzzdir, executable, executable));
+    }
+
+    // For each of the corpus directories, zip it and place the archive next to the fuzz target.
+    let corporas = await fsp.readdir("./corpus", {withFileTypes: true})
+    for (let corpus of corporas) {
+      if (corpus.isDirectory()) {
+        await zip(path.join("./corpus", corpus.name), ".", path.join(fdfuzzdir, corpus.name, `${corpus.name}.zip`))
+      }
+    }
+
+    // Copy the shared objects
+    await fsp.cp("./opt/lib", `${fdfuzzdir}/lib`, {dereference: false, recursive: true})
 
     // [1] Zip the artifact directory
     core.debug(`creating zip archive from ${fdfuzzdir}`)
-
-    var promiseResolve : (value : unknown) => void
-    var promiseReject : (reason? : any) => void;
-    var promise = new Promise(function(resolve, reject){
-      promiseResolve = resolve;
-      promiseReject = reject;
-    });
-
-    let childProcess = exec(`zip -r fuzztargets.zip ${fdfuzzdir}`, (err, stdout, stderr) => {
-      if (err) {
-        console.error(`[stderr] zip: ${stderr}`)
-        console.error(`[stdout] zip: ${stdout}`)
-        core.setFailed(err)
-        promiseReject(err)
-        return
-      }
-      promiseResolve(null)
-    })
-    await promise;
+    await zip(fdfuzzdir, ".", path.join(__dirname, "fuzztargets.zip"))
 
 
     // [2] Upload the artifact to GCS
@@ -79,7 +78,7 @@ async function run(): Promise<void> {
     // [2.2] Write the object
     let bucket = gcs.bucket(bucketName)
     const dstObject = bucket.file(objectPath)
-    let fileStream = fs.createReadStream('fuzztargets.zip')
+    let fileStream = fs.createReadStream(`${__dirname}/fuzztargets.zip`)
 
     let streamFileUpload = new Promise((resolve, reject) => {
       fileStream.pipe(dstObject.createWriteStream()).on('finish', resolve).on('error', reject)
@@ -90,6 +89,58 @@ async function run(): Promise<void> {
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
+}
+
+async function rewriteRPATH(executablePath: string) {
+  var promiseResolve : (value : unknown) => void
+  var promiseReject : (reason? : any) => void;
+  var promise = new Promise(function(resolve, reject){
+    promiseResolve = resolve;
+    promiseReject = reject;
+  });
+
+  console.log(`running: patchelf ${executablePath} --set-rpath $ORIGIN/../lib/`)
+  let childProcess = exec(
+    `patchelf ${executablePath} --set-rpath '$ORIGIN'/../lib/`,
+    (err, stdout, stderr) => {
+    if (err) {
+      console.error(`[stderr] patchelf: ${stderr}`)
+      console.error(`[stdout] patchelf: ${stdout}`)
+      core.setFailed(err)
+      promiseReject(err)
+      return
+    }
+    promiseResolve(null)
+  })
+  await promise;
+}
+
+async function zip(workingDirectory: fs.PathLike, targetDirectory: fs.PathLike, dst: fs.PathLike) {
+  var promiseResolve : (value : unknown) => void
+  var promiseReject : (reason? : any) => void;
+  var promise = new Promise(function(resolve, reject){
+    promiseResolve = resolve;
+    promiseReject = reject;
+  });
+
+  console.log(`running: "zip -r ${dst} ." from ${workingDirectory}`)
+  let childProcess = exec(
+    `zip -r ${dst} ${targetDirectory}`,
+    {
+      cwd: workingDirectory.toString()
+    },
+    (err, stdout, stderr) => {
+    if (err) {
+      console.error(`[stderr] zip: ${stderr}`)
+      console.error(`[stdout] zip: ${stdout}`)
+      core.setFailed(err)
+      promiseReject(err)
+      return
+    }
+    promiseResolve(null)
+  })
+
+  return promise;
 }
 
 run()
